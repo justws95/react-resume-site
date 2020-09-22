3a9546c08c15d00b65f5f5b8e250ba2cf7a89461
Justin <justws95@gmail.com>
2020-09-22 00:14:35 -0400

    Starting to build out file structure for proejct. Getting my feet wet.

---
+++ src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="Landing-Page-header">
+      </header>
+      <body>
+          <p>
+              This is eventually where some content will go!!!!
+          </p>
+      </body>
+    </div>
+  );
+}
+
+export default LandingPage;
